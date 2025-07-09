49aca27101e58c9f7ce51cbe1185ed9373fbc1f1
ekstazzz0 <tapok.dit@gmail.com>
2025-07-09 23:14:19 +0400

    updated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,36 +2,64 @@ import pytest
 from sqlmodel import create_engine, SQLModel, Session
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
+import os
+from datetime import datetime
 
-from app.main import app, lifespan
+os.environ["APP_ENV"] = "test"
+
+from app.main import app
 from app.db.session import get_session
+from app.db.models import Doctor, Appointment
+from datatest import doctor_for_db, appointment_description, appointment_time
 
-test_engine = create_engine("sqlite:///:memory:", echo=False, connect_args={"check_same_thread": False})
+@pytest.fixture
+def prepared_doctor():
+    session_generator = get_session()
+    session = next(session_generator)
+    db_doctor = Doctor(**doctor_for_db)
 
+    session.add(db_doctor)
+    session.commit()
+    session.refresh(db_doctor)
 
-@pytest.fixture(scope="session")
-def init_db():
-    SQLModel.metadata.create_all(test_engine)
-    yield
+    yield db_doctor
 
+    session.delete(db_doctor)
+    session.commit()
 
-async def override_lifespan(app: FastAPI):
-    init_db()
-    yield
-
-
-
-def override_get_session():
-    with Session(test_engine) as session:
-        yield session
+    try:
+        next(session_generator)
+    except StopIteration:
+        pass
 
 
 @pytest.fixture
-def client(init_db):
-    app.dependency_overrides[get_session] = override_get_session
-    app.dependency_overrides[lifespan] = override_lifespan
+def prepared_appointment(prepared_doctor):
+    session_generator = get_session()
+    session = next(session_generator)
 
-    with TestClient(app=app, base_url="http://test") as c:
+    db_appointment = Appointment(
+        doctor_id=prepared_doctor.id,
+        description=appointment_description,
+        start_time=appointment_time
+    )
+
+    session.add(db_appointment)
+    session.commit()
+    session.refresh(db_appointment)
+
+    yield db_appointment
+
+    session.delete(db_appointment)
+    session.commit()
+
+    try:
+        next(session_generator)
+    except StopIteration:
+        pass
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as c:
         yield c
-    
-    app.dependency_overrides.clear()
--- tests/test_1_doctors.py
+++ tests/test_1_doctors.py
@@ -1,24 +1,34 @@
 from fastapi.testclient import TestClient
-from datatest import doctor
+import random
+
+from datatest import doctor_for_response, doctor_error_create, doctor_error_create_response
 
 def test_1_create_doctor(client: TestClient):
-    response = client.post("/doctors", json=doctor)
-    print(response.text)
+    response = client.post("/doctors", json=doctor_for_response)
     assert response.status_code == 201
     data = response.json()
     assert data["id"] == 1
     data.pop("id")
-    assert data == doctor
+    assert data == doctor_for_response
 
 
-# def test_2_get_doctor(client: TestClient):
-#     response = client.get("/doctors/1")
-#     assert response.status_code == 200
-#     data = response.json().pop("id")
-#     assert data == doctor
+def test_2_create_error_doctor(client: TestClient):
+    response = client.post("/doctors", json=doctor_error_create)
+    assert response.status_code  == 422
+    assert response.json() == doctor_error_create_response
 
 
-# def test_3_error_get_doctor(client: TestClient):
-#     response = client.get("/doctors/0")
-#     assert response.status_code == 404
-#     assert response.json() == {"detail": "Doctor with id 0 was not found in database."}
+def test_3_get_doctor(client: TestClient, prepared_doctor):
+    response = client.get("/doctors/1")
+    assert response.status_code == 200
+    data = response.json()
+    data.pop("id")
+    assert data == doctor_for_response
+
+
+def test_4_get_error_doctor(client: TestClient):
+    for _ in range(10):
+        doctor_id = random.randint(-5,5)
+        response = client.get(f"/doctors/{doctor_id}")
+        assert response.status_code == 404
+        assert response.json() == {'detail': f'Doctor with id {doctor_id} was not found in database.'}
