efd94584b6a0b1256100369e90329e0e09c0ee0b
ekstazzz0 <tapok.dit@gmail.com>
2025-07-10 23:16:49 +0400

    fixed by isort, black and flake 8

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,16 +1,16 @@
 import os
-import sys
+
 from fastapi import FastAPI
 
+from app.core.config import application_environment
 from app.db.repository import init_db
 from app.db.session import engine
-from app.core.config import application_environment
 
 
 async def lifespan(app: FastAPI):
     init_db()
     yield
 
-    if application_environment == "test" or any("pytest" in arg for arg in sys.argv):
+    if application_environment == "test":
         engine.dispose()
         os.unlink("./temp.db")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
+from app.core.utils import lifespan
 from app.endpoints.appointments import router as appointments_router
 from app.endpoints.doctors import router as doctor_router
-from app.core.utils import lifespan
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(appointments_router)
