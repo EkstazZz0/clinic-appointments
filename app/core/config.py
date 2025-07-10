b5071abdcf86b85c77ef4a49c5538009692d044f
ekstazzz0 <tapok.dit@gmail.com>
2025-07-11 00:23:01 +0400

    updated for correct mypy, added mypy to requirements.txt

--- app/core/config.py
+++ app/core/config.py
@@ -1,29 +1,33 @@
 import os
 import sys
+from typing import Any
 
 if any("pytest" in arg for arg in sys.argv):
     os.environ["APP_ENV"] = "test"
 
 application_environment = os.getenv("APP_ENV")
 
-if application_environment == "test":
-    os.environ["APP_ENV"] = "test"
-    db_connect_configuration = {
-        "url": "sqlite:///temp.db",
-        "connect_args": {"check_same_thread": False},
-        "echo": True,
-    }
-elif application_environment == "production":
-    try:
-        db_connect_configuration = {
-            "url": "postgresql+psycopg2://"
-            + os.environ["DB_USER"]
-            + ":"
-            + os.environ["DB_PASSWORD"]
-            + "@db:5432/"
-            + os.environ["DB_NAME"]
+def get_db_configuration() -> dict[str, Any]:
+
+    if application_environment == "test":
+        return{
+            "url": "sqlite:///temp.db",
+            "connect_args": {"check_same_thread": False},
+            "echo": True,
         }
-    except KeyError as e:
-        raise KeyError(f"Variable {e.args[0]} must be specified")
-else:
-    raise ValueError("variable APP_ENV must be specified as test or production")
+    elif application_environment == "production":
+        try:
+            return {
+                "url": "postgresql+psycopg2://"
+                + os.environ["DB_USER"]
+                + ":"
+                + os.environ["DB_PASSWORD"]
+                + "@db:5432/"
+                + os.environ["DB_NAME"]
+            }
+        except KeyError as e:
+            raise KeyError(f"Variable {e.args[0]} must be specified")
+    else:
+        raise ValueError("variable APP_ENV must be specified as test or production")
+
+db_connect_configuration = get_db_configuration()
